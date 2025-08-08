54af309082afe526e98559a801bee4b5a427175e
Júnior Bandeira <bandeirajunior150@gmail.com>
2025-08-07 21:13:55 -0300

    Exception handling - findById

---
+++ src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.educandoweb.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+    private static final String message = "Resource not found. Id: ";
+
+    public ResourceNotFoundException(Object id) {
+        super(message + id);
+    }
+}
